2bf4835268f8e66ff4e3c7d336b0a596a3b84314
Tim <sterzeltim@gmail.com>
2016-11-10 20:04:38 +0100

    Implement important getter and setters

--- include/Components/MoveableBlock.hpp
+++ include/Components/MoveableBlock.hpp
@@ -7,10 +7,29 @@ namespace gsf
     class MoveableBlock
     {
         private:
-            sf::Vector2f m_position;
+            sf::Vector2f m_pos;
+            sf::Vector2f m_lastPos;
+
+            float m_width;
+            float m_height;
         public:
+
             MoveableBlock();
             virtual ~MoveableBlock();
+
+            sf::Vector2f getPosition() const;
+            void setPosition(float x, float y);
+            void setPosition(sf::Vector2f pos);
+            sf::Vector2f getLastPosition() const;
+
+            float getWidth() const;
+            void setWidth(float width);
+            float getHeight() const;
+            void setHeight(float height);
+
+            void move(float x, float y);
+            void move(sf::Vector2f xy);
+
     };
 
 }
--- src/Components/MoveableBlock.cpp
+++ src/Components/MoveableBlock.cpp
@@ -10,3 +10,55 @@ gsf::MoveableBlock::~MoveableBlock()
 {
 
 }
+
+sf::Vector2f gsf::MoveableBlock::getPosition() const
+{
+    return m_pos;
+}
+
+void gsf::MoveableBlock::setPosition(float x, float y)
+{
+    m_lastPos = m_pos;
+    m_pos.x = x;
+    m_pos.y = y;
+}
+
+void gsf::MoveableBlock::setPosition(sf::Vector2f pos)
+{
+    setPosition(pos.x, pos.y);
+}
+
+sf::Vector2f gsf::MoveableBlock::getLastPosition() const
+{
+    return m_lastPos;
+}
+
+float gsf::MoveableBlock::getWidth() const
+{
+    return m_width;
+}
+
+void gsf::MoveableBlock::setWidth(float width)
+{
+    m_width = width;
+}
+
+float gsf::MoveableBlock::getHeight() const
+{
+    return m_height;
+}
+
+void gsf::MoveableBlock::setHeight(float height)
+{
+    m_height = height;
+}
+
+void gsf::MoveableBlock::move(float x, float y)
+{
+    setPosition(m_pos.x + x, m_pos.y + y);
+}
+
+void gsf::MoveableBlock::move(sf::Vector2f xy)
+{
+    move(xy.x, xy.y);
+}
