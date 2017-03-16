2978c6c6c695cb7a8b6f130cfb5768915cdce366
TmCrafz <sterzeltim@gmail.com>
2017-03-16 23:01:17 +0100

    Add example

---
+++ examples/Orientation/orientation.cpp
@@ -0,0 +1,58 @@
+#include "GUI-SFML.hpp"
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window{ sf::VideoMode{ 1280, 720 }, "GUI-SFML Example Orientation" };
+    // Some widgets need a Font to work properly, so we load one
+    sf::Font font;
+    if (!font.loadFromFile("../../assets/fonts/LiberationSans-Regular.ttf"))
+    {
+        std::cout << "Error by loading Font" << std::endl;
+    }
+    // Create an instance of GUIEnvironment which handle the drawing, updating and 
+    // events of the widgets which are added to it
+    // It needs the window as a parameter
+    gsf::GUIEnvironment environment{ window };
+    
+    // Create a Vertical layout. The "create" method create a unique_ptr with the
+    // given parameters.
+    // The width and height of the layout is automatic calculated and depends
+    // of its child widgets
+    gsf::VerticalLayout::Ptr layout{ gsf::VerticalLayout::create() };
+    layout->setPosition(300.f, 300.f);
+    // Make background gray
+    layout->setBackgroundColor(sf::Color{ 192, 192, 192 });
+    
+    // Create some TextWidgets
+    gsf::TextWidget::Ptr text1{ gsf::TextWidget::create("Text One", font) };
+    gsf::TextWidget::Ptr text2{ gsf::TextWidget::create("Text Two", font) };
+    gsf::TextWidget::Ptr text3{ gsf::TextWidget::create("Text Three", font) };
+    // Add TextWidgets to the Layout
+    layout->attachChild(std::move(text1));
+    layout->attachChild(std::move(text2));
+    layout->attachChild(std::move(text3));
+
+    // Add Layout to environment
+    environment.addWidget(std::move(layout));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+            environment.handleEvent(event);
+        }
+        //environment.update(dt);
+
+        window.clear(sf::Color::White);
+        window.draw(environment);
+        window.display();
+    }
+
+}
