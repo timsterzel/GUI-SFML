cced320c4a2ec4c827109b2b6b1120e8c6ece8dd
TmCrafz <sterzeltim@gmail.com>
2017-04-05 15:05:27 +0200

    Move GUI-SFML.hpp

---
+++ include/GUI-SFML.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef GUI_SFML_HPP
+#define GUI_SFML_HPP
+
+#include "Widgets/ButtonWidget.hpp"
+#include "Widgets/CheckBoxWidget.hpp"
+#include "Widgets/ComboBoxWidget.hpp"
+#include "Widgets/ConsoleWidget.hpp"
+#include "GUIEnvironment.hpp"
+#include "Widgets/ListBoxWidget.hpp"
+#include "Orientation.hpp"
+#include "Widgets/ProgressWidget.hpp"
+#include "Widgets/ScrollableWidget.hpp"
+#include "Widgets/TextButtonWidget.hpp"
+#include "Widgets/TextInputWidget.hpp"
+#include "Widgets/TextWidget.hpp"
+#include "Widgets/VerticalLayoutWidget.hpp"
+#include "Widgets/Widget.hpp"
+#include "Widgets/WindowWidget.hpp"
+
+#endif // GUI_SFML_HPP
